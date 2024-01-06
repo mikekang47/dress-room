895b9c45475d78dca8e3016cc9922517a1e242f8
sihoo kang <sihookang47@gmail.com>
2024-01-07 02:05:29 +0900

    Feat: set env

---
+++ src/main/java/com/problemsolver/dressroom/DressRoomApplication.java
@@ -0,0 +1,13 @@
+package com.problemsolver.dressroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DressRoomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DressRoomApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/problemsolver/dressroom/DressRoomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.problemsolver.dressroom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DressRoomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
